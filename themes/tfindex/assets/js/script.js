93a911a3d1e443eb6974d0f63afe33ae3174c78d
PhuongHX <xuanphuong3105@gmail.com>
2022-03-06 23:01:30 +0700

    Handle input currency

--- themes/tfindex/assets/js/script.js
+++ themes/tfindex/assets/js/script.js
@@ -1,3 +1,35 @@
-jQuery(document).ready(function($) {
-    console.log('Loaded js!')
-});
+// eslint-disable-next-line no-undef
+jQuery( document ).ready( function( $ ) {
+	'use strict';
+
+	// Format currency input
+	$( function() {
+		const $form = $( '.wpforms-form' );
+		const $input = $form.find( '.form-money input' );
+
+		$input.on( 'keyup', function( event ) {
+			// When user select text in the document, also abort.
+			const selection = window.getSelection().toString();
+			if ( selection !== '' ) {
+				return;
+			}
+
+			// When the arrow keys are pressed, abort.
+			if ( $.inArray( event.keyCode, [ 38, 40, 37, 39 ] ) !== -1 ) {
+				return;
+			}
+
+			const $this = $( this );
+
+			// Get the value.
+			let input = $this.val();
+
+			input = input.replace( /[\D\s\._\-]+/g, '' );
+			input = input ? parseInt( input, 10 ) : 0;
+
+			$this.val( function() {
+				return ( input === 0 ) ? '' : input.toLocaleString( 'en-US' );
+			} );
+		} );
+	} );
+} );

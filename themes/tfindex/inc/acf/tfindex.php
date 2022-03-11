960a9903cc55e785954b5bd1dcfbea6a0d7071e2
PhuongHX <xuanphuong3105@gmail.com>
2022-03-11 21:17:38 +0700

    Update admin

---
+++ themes/tfindex/inc/acf/tfindex.php
@@ -0,0 +1,92 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_tfindex',
+        'title' => 'TFindex',
+        'fields' => array(
+            array(
+                'key' => 'field_tfindex_sub_header',
+                'label' => 'Sub header',
+                'name' => 'sub_header',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_tfindex_description',
+                'label' => 'Description',
+                'name' => 'description',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_tfindex_icon',
+                'label' => 'Icon',
+                'name' => 'icon',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'url',
+                'preview_size' => 'medium',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'tfindexs',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+    ));
+
+endif;
---
+++ themes/tfindex/inc/custom-post/tfindex.php
@@ -0,0 +1,63 @@
+<?php
+
+if ( ! function_exists('tfindexs_post_type') ) {
+
+// Register Custom Post Type
+    function tfindexs_post_type() {
+
+        $labels = array(
+            'name'                  => _x( 'TFindex', 'Post Type General Name', 'tfindex' ),
+            'singular_name'         => _x( 'TFindex', 'Post Type Singular Name', 'tfindex' ),
+            'menu_name'             => __( 'TFindex', 'tfindex' ),
+            'name_admin_bar'        => __( 'TFindex', 'tfindex' ),
+            'archives'              => __( 'Item Archives', 'tfindex' ),
+            'attributes'            => __( 'Item Attributes', 'tfindex' ),
+            'parent_item_colon'     => __( 'Parent Item:', 'tfindex' ),
+            'all_items'             => __( 'All Items', 'tfindex' ),
+            'add_new_item'          => __( 'Add New Item', 'tfindex' ),
+            'add_new'               => __( 'Add New', 'tfindex' ),
+            'new_item'              => __( 'New Item', 'tfindex' ),
+            'edit_item'             => __( 'Edit Item', 'tfindex' ),
+            'update_item'           => __( 'Update Item', 'tfindex' ),
+            'view_item'             => __( 'View Item', 'tfindex' ),
+            'view_items'            => __( 'View Items', 'tfindex' ),
+            'search_items'          => __( 'Search Item', 'tfindex' ),
+            'not_found'             => __( 'Not found', 'tfindex' ),
+            'not_found_in_trash'    => __( 'Not found in Trash', 'tfindex' ),
+            'featured_image'        => __( 'Featured Image', 'tfindex' ),
+            'set_featured_image'    => __( 'Set featured image', 'tfindex' ),
+            'remove_featured_image' => __( 'Remove featured image', 'tfindex' ),
+            'use_featured_image'    => __( 'Use as featured image', 'tfindex' ),
+            'insert_into_item'      => __( 'Insert into item', 'tfindex' ),
+            'uploaded_to_this_item' => __( 'Uploaded to this item', 'tfindex' ),
+            'items_list'            => __( 'Items list', 'tfindex' ),
+            'items_list_navigation' => __( 'Items list navigation', 'tfindex' ),
+            'filter_items_list'     => __( 'Filter items list', 'tfindex' ),
+        );
+        $args = array(
+            'label'                 => __( 'TFindex', 'tfindex' ),
+            'description'           => __( 'What client say?', 'tfindex' ),
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor', 'custom-fields', 'thumbnail' ),
+//			'taxonomies'            => array( 'category', 'post_tag' ),
+            'hierarchical'          => false,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 4,
+            'menu_icon'             => 'dashicons-analytics',
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => false,
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'rewrite'               => true,
+            'capability_type'       => 'page',
+        );
+        register_post_type( 'tfindex', $args );
+
+    }
+    add_action( 'init', 'tfindexs_post_type', 0 );
+
+}

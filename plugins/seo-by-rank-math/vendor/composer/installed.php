a116089288155b8e078ae8cff78ed9328b05bb03
PhuongHX <xuanphuong3105@gmail.com>
2022-03-20 16:48:45 +0700

    Fix

---
+++ plugins/seo-by-rank-math/vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bbc4d9e1dfd70f7a3e5e23402a5d3100775aefe6',
+    'name' => 'rankmath/seo-by-rank-math',
+  ),
+  'versions' => 
+  array (
+    'a5hleyrich/wp-background-processing' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2cbee1abd1b49e1133cd8f611df4d4fc5a8b9800',
+    ),
+    'cmb2/cmb2' => 
+    array (
+      'pretty_version' => 'v2.10.1',
+      'version' => '2.10.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4afc4bb7b92ab6d93aac2247c9a84af773e42532',
+    ),
+    'donatj/phpuseragentparser' => 
+    array (
+      'pretty_version' => 'v1.5.0',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cc9d872cddfc180c52d084d0dff1e4aad653d37f',
+    ),
+    'mythemeshop/wordpress-helpers' => 
+    array (
+      'pretty_version' => 'v1.1.18',
+      'version' => '1.1.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6294c995e9365bee25fcfa8f3c045800620a5278',
+    ),
+    'rankmath/seo-by-rank-math' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bbc4d9e1dfd70f7a3e5e23402a5d3100775aefe6',
+    ),
+    'woocommerce/action-scheduler' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3218a33ff14b968f8cb05de9656c2efa1eeb1330',
+    ),
+  ),
+);
